dd865dfae0f19da4c718a77fa50f60076cffaecd
Administrator <Administrator@hp-PC>
2019-04-28 14:51:33 +0800

    哈哈哈

---
+++ shop-common/src/main/java/cn/icloudit/common/api/enity/BaseEntity.java
@@ -0,0 +1,18 @@
+package cn.icloudit.common.api.enity;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseEntity {
+
+	private Integer id;
+	private Timestamp created;
+	private Timestamp updated;
+	
+
+	
+}
---
+++ shop-member-dao/src/main/java/cn/icloudit/dao/IUserDao.java
@@ -0,0 +1,10 @@
+package cn.icloudit.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import cn.icloudit.common.mybatis.IBaseDAO;
+
+@Mapper
+public interface IUserDao extends IBaseDAO {
+
+}
